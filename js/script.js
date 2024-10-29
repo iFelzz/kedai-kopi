600fc994d2c81acd8e46fd583142f3d58eb38090
muhfelmi <felmiadi@gmail.com>
2024-10-29 08:58:54 +0800

    Added UI for Search Form

--- js/script.js
+++ js/script.js
@@ -1,13 +1,26 @@
-// Toggle Class Active
+// Toggle Class Active Hamburger Menu
 const navbarNav = document.querySelector(".navbar-nav");
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// Tutup sidebar dari luar
-const hamburger = document.querySelector("#hamburger-menu");
+// Toggle Class Active Search Form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  e.preventDefault();
+};
+
+// Klik diluar elemen
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
