4c68c02423d6ffe179fc4b8d33af28ce6c23954f
muhfelmi <felmiadi@gmail.com>
2024-10-30 07:48:53 +0800

    Added UI for Shopping Cart

--- js/script.js
+++ js/script.js
@@ -13,9 +13,17 @@ document.querySelector("#search-button").onclick = (e) => {
   e.preventDefault();
 };
 
+// Toggle Class Active Shopping Cart
+const shoppingCart = document.querySelector(".shopping-cart");
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
+
 // Klik diluar elemen
 const hm = document.querySelector("#hamburger-menu");
 const sb = document.querySelector("#search-button");
+const sc = document.querySelector("#shopping-cart-button");
 document.addEventListener("click", function (e) {
   if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
@@ -23,5 +31,9 @@ document.addEventListener("click", function (e) {
 
   if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
     searchForm.classList.remove("active");
+  }
+
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
   }
 });
