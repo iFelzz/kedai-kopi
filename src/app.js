79428083f15a960f77eb82a88644629e1e699446
muhfelmi <felmiadi@gmail.com>
2024-11-04 10:59:09 +0800

    Added Payment Gateway Midtrans Demo

--- src/app.js
+++ src/app.js
@@ -71,6 +71,60 @@ document.addEventListener("alpine:init", () => {
   });
 });
 
+// Form Validation
+const checkoutButton = document.querySelector(".checkout-button");
+checkoutButton.disabled = true;
+
+const form = document.querySelector("#checkoutForm");
+form.addEventListener("keyup", function () {
+  for (let i = 0; i < form.elements.length; i++) {
+    if (form.elements[i].value.length !== 0) {
+      checkoutButton.classList.remove("disabled");
+      checkoutButton.classList.add("disabled");
+    } else {
+      return false;
+    }
+  }
+  checkoutButton.disabled = false;
+  checkoutButton.classList.remove("disabled");
+});
+
+// Kirim data ketika button pay now di klik
+checkoutButton.addEventListener("click", async function (e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const data = new URLSearchParams(formData);
+  const objData = Object.fromEntries(data);
+  // const message = formatMessage(objData);
+  // window.open("https://wa.me/6285798889727?text=" + encodeURIComponent(message));
+
+  // Minta Transaction Token dengan ajax/fetch
+  try {
+    const response = await fetch("php/placeOrder.php", {
+      method: "POST",
+      body: data,
+    });
+    const token = await response.text();
+    console.log(token);
+    window.snap.pay(token);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+// Format pesan whatsapp
+const formatMessage = (obj) => {
+  return `Data Customer
+  Nama : ${obj.name}
+  Email : ${obj.email}
+  No Hp : ${obj.phone}
+Data Pesanan
+  ${JSON.parse(obj.items).map((item) => `${item.name} (${item.quantity} x ${rupiah(item.total)}) \n`)}
+  TOTAL : ${rupiah(obj.total)}
+  Terima Kasih!
+  `;
+};
+
 // Mata uang rupiah
 const rupiah = (number) => {
   return new Intl.NumberFormat("id-ID", {
